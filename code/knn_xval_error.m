e539aa8404b236e513f9288b39ff510e34c56383
Akshay Chandrasekhar <Akshay Chandrasekhar>
2016-09-22 19:49:14 -0400

    knn_xval_erro yo

--- code/knn_xval_error.m
+++ code/knn_xval_error.m
@@ -14,6 +14,31 @@ function [error] = knn_xval_error(K, X, Y, part, distFunc)
 %
 % SEE ALSO
 %   MAKE_XVAL_PARTITION, KNN_TEST
+z = max(part)
+sumz = zeros(z,1)
+for i = 1:z
+    parttest = part==i;
+    parttrain = part~=i;
+    [m,n] = size(X);
+    number = [1:m];
 
-% FILL IN YOUR CODE HERE
+    vtest = number.*parttest;
+    vtest = vtest(vtest~=0);
+    test = X(vtest,:);
+    Ytest = Y(vtest,:);
 
+    vtrain = number.*parttrain;
+    vtrain = vtrain(vtrain~=0);
+    train = X(vtrain,:);
+    Ytrain = Y(vtrain,:);
+
+    testLabels = knn_test(K,train,Ytrain,test,distFunc);
+    difference = testLabels-Ytest;
+    difference = difference~=0;
+    j = sum(difference);
+    [m,n] = size(difference);
+    sumz(i) = j/m;
+end
+sumz;
+error =(sum(sumz))/z
+%testLabels = knn_test(K,X,Y,test0,'l2');
